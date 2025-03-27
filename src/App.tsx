d292a5e61f2f0bff5737d50008018cc98cffb7c8
Craig Clayton <craig__clayton@outlook.com>
2025-03-27 21:04:27 +0000

    render summat on screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+
+interface Todo {
+  id: string;
+  title: string;
+  done: boolean;
+}
+
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([
+    { id: crypto.randomUUID(), title: 'Do washing', done: false },
+  ]);
+
   return (
     <>
-      <div></div>
+      <div>
+        {todos.map((todo) => (
+          <p>{todo.title}</p>
+        ))}
+      </div>
     </>
   );
 }
